3a9ed70afafe0d92a17af8249bb2076f7a54a5c5
919642307 <919642307@qq.com>
2021-02-16 15:07:34 +0800

    Update SLList.java

--- lists2/SLList.java
+++ lists2/SLList.java
@@ -1,4 +1,5 @@
- /** An SLList is a list of integers, which hides the terrible truth
+
+/** An SLList is a list of integers, which hides the terrible truth
    * of the nakedness within. */
 public class SLList {	
 	private static class IntNode {
@@ -8,7 +9,7 @@ public class SLList {
 		public IntNode(int i, IntNode n) {
 			item = i;
 			next = n;
-			System.out.println(size);
+
 		}
 	} 
 
@@ -16,10 +17,7 @@ public class SLList {
 	private IntNode sentinel;
 	private int size;
 
-	private static void lectureQuestion() {
-		SLList L = new SLList();
-		IntNode n = IntNode(5, null);
-	}
+
 
 	/** Creates an empty SLList. */
 	public SLList() {
@@ -33,12 +31,31 @@ public class SLList {
 		size = 1;
 	}
 
+	public SLList(int[] array){
+		sentinel = new IntNode(63,null);
+		IntNode p =sentinel;
+		for(int i = 0;i<array.length;i++){
+			p.next = new IntNode(array[i],null);
+			p = p.next;
+		}
+		size = array.length;
+	}
  	/** Adds x to the front of the list. */
  	public void addFirst(int x) {
  		sentinel.next = new IntNode(x, sentinel.next);
  		size = size + 1;
  	}
 
+ 	public void deleteFirst(){
+ 		if (size ==0){
+ 			return;
+		}
+ 		else {
+			sentinel.next = sentinel.next.next;
+			size -= 1;
+		}
+
+	}
  	/** Returns the first item in the list. */
  	public int getFirst() {
  		return sentinel.next.item;
@@ -65,8 +82,15 @@ public class SLList {
 
 	public static void main(String[] args) {
  		/* Creates a list of one integer, namely 10 */
- 		SLList L = new SLList();
- 		L.addLast(20);
+		int[] a = new int[]{1,2,3,4};
+ 		SLList L = new SLList(a);
+
+
+		System.out.println(L.getFirst());
  		System.out.println(L.size());
- 	}
+ 		L.deleteFirst();
+		System.out.println(L.getFirst());
+		System.out.println(L.size());
+
+	}
 }
